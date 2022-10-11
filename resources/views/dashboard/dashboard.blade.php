b1341d0635606670293ff5e2af35f1e0d33e038d
MohamedGomati7 <mohamedgomati99@gmail.com>
2022-10-11 22:40:17 +0300

    BackEnd

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -63,7 +63,7 @@
             </div>
             <span class="nav-link-text ms-1">applicants</span>
           </a>
-        </li>
+        </li> 
         <li class="nav-item">
           <a class="nav-link text-white " href="/dashboard/news">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
